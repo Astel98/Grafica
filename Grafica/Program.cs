a39eaf1ab56e756283e80a96124dce7cc82b7b83
Astel98 <Astel98@users.noreply.github.com>
2020-12-11 20:30:20 -0400

    asd

---
+++ Grafica/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new Game(800, 600, "LearnOpenTK - Hello Triangle!"))
+            {
+                window.Run(60.0);
+            }
+        }
+    }
+}
